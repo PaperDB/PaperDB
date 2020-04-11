12980751e3d74e9908f2d5651caa883546f3f963
make1app <makeoneapp@gmail.com>
2020-04-11 06:30:53 -0400

    change package name to @paper-db/paper-db

--- src/types/type-registry.ts
+++ src/types/type-registry.ts
@@ -47,7 +47,7 @@ export const CONVERTERS = [
  * Register custom type converters to PaperDB:
  * @example
  * ```
- *   import { TypeConverter, TYPE_REGISTRY } from 'paper-db/types'
+ *   import { TypeConverter, TYPE_REGISTRY } from '@paper-db/paper-db/types'
  *   
  *   // create a custom type converter
  *   `@`TypeConverter.Conversion() // the decorator is for type checking, can be safely omitted
@@ -59,7 +59,7 @@ export const CONVERTERS = [
  *   TYPE_REGISTRY.add(Type1)
  *   
  *   // add into the TypeRegistration interface
- *   declare module 'paper-db/types' {
+ *   declare module '@paper-db/paper-db/types' {
  *     interface TypeRegistration {
  *       'type1': TypeConverter.Converter;
  *     }
