02caa1061ce7175b4e92ebb3c48da5be1432599e
make1app <makeoneapp@gmail.com>
2020-04-14 01:25:55 -0400

    test: TypeConverter validation

--- test/types/validator.test.ts
+++ test/types/validator.test.ts
@@ -1,6 +1,7 @@
 
 import type { PaperDB } from '../../src'
 import { createValidator } from '../../src/types/validator'
+import { isValidTypeConverter } from '../../src/types/converter'
 import { ConverterNested, NestedObj, ConverterTest1V2 } from './mock'
 import { mockPaperdb } from '../mock'
 
@@ -41,6 +42,7 @@ describe('PaperDB type validator', () => {
   let obj: NestedObj
   beforeAll(async () => {
     obj = await new ConverterNested(new ConverterTest1V2()).toTypedObj()
+    expect(obj).toStrictEqual({ '$type': 'nested', '$v': 0, test1: { '$type': 'test1', '$v': 2, def: '456' } })
   })
 
   describe('create', () => {
@@ -60,5 +62,27 @@ describe('PaperDB type validator', () => {
   describe('when the TypeConverter is with `isValidTypedObj`', () => {
     const validator = createValidator(Converter1, mockPaperdb)
     tests(validator, () => obj)
+  })
+
+  describe('TypeConverter validation', () => {
+    test('return false if the TypeConverter is invalid', () => {
+      expect(isValidTypeConverter(undefined as any)).toBe(false)
+      expect(isValidTypeConverter(1 as any)).toBe(false)
+      expect(isValidTypeConverter({} as any)).toBe(false)
+      expect(isValidTypeConverter({ $type: 'test', $v: 'str' } as any)).toBe(false)
+      expect(isValidTypeConverter({ $type: 'test', $v: 1, fromTypedObj: 'str' } as any)).toBe(false)
+    })
+
+    test('return true if the TypeConverter is valid, no type name provided', () => {
+      expect(isValidTypeConverter(ConverterNested)).toBe(true)
+    })
+
+    test('return false if the TypeConverter is valid, but does not match the type name given', () => {
+      expect(isValidTypeConverter(ConverterNested, 'test')).toBe(false)
+    })
+
+    test('return true if the TypeConverter is valid, and matches the type name provided', () => {
+      expect(isValidTypeConverter(ConverterNested, ConverterNested.$type)).toBe(true)
+    })
   })
 })
