b2e5542b0ac876aa50d55b4efc21ac4272fff335
make1app <makeoneapp@gmail.com>
2020-04-15 05:58:48 -0400

    fix: load the IndexedDB shim
    using Object.defineProperty, as properties on the globalThis (window, global, self) object is non-writable

--- src/shim.ts
+++ src/shim.ts
@@ -6,6 +6,37 @@
 
 import { globalthis, env } from './utils'
 
+import fakeIndexedDB from 'fake-indexeddb/build/fakeIndexedDB'
+import FDBCursor from 'fake-indexeddb/build/FDBCursor'
+import FDBCursorWithValue from 'fake-indexeddb/build/FDBCursorWithValue'
+import FDBDatabase from 'fake-indexeddb/build/FDBDatabase'
+import FDBFactory from 'fake-indexeddb/build/FDBFactory'
+import FDBIndex from 'fake-indexeddb/build/FDBIndex'
+import FDBKeyRange from 'fake-indexeddb/build/FDBKeyRange'
+import FDBObjectStore from 'fake-indexeddb/build/FDBObjectStore'
+import FDBOpenDBRequest from 'fake-indexeddb/build/FDBOpenDBRequest'
+import FDBRequest from 'fake-indexeddb/build/FDBRequest'
+import FDBTransaction from 'fake-indexeddb/build/FDBTransaction'
+import FDBVersionChangeEvent from 'fake-indexeddb/build/FDBVersionChangeEvent'
+
+const loadShim = (): void => {
+  // replace `require('fake-indexeddb/auto.js')`
+  // using Object.defineProperty, 
+  // as properties on the globalThis (window, global, self) object is non-writable
+  Object.defineProperty(globalthis, 'indexedDB', { value: fakeIndexedDB })
+  Object.defineProperty(globalthis, 'IDBCursor', { value: FDBCursor })
+  Object.defineProperty(globalthis, 'IDBCursorWithValue', { value: FDBCursorWithValue })
+  Object.defineProperty(globalthis, 'IDBDatabase', { value: FDBDatabase })
+  Object.defineProperty(globalthis, 'IDBFactory', { value: FDBFactory })
+  Object.defineProperty(globalthis, 'IDBIndex', { value: FDBIndex })
+  Object.defineProperty(globalthis, 'IDBKeyRange', { value: FDBKeyRange })
+  Object.defineProperty(globalthis, 'IDBObjectStore', { value: FDBObjectStore })
+  Object.defineProperty(globalthis, 'IDBOpenDBRequest', { value: FDBOpenDBRequest })
+  Object.defineProperty(globalthis, 'IDBRequest', { value: FDBRequest })
+  Object.defineProperty(globalthis, 'IDBTransaction', { value: FDBTransaction })
+  Object.defineProperty(globalthis, 'IDBVersionChangeEvent', { value: FDBVersionChangeEvent })
+}
+
 const needShim = async (): Promise<boolean> => {
   if (env.isBrowser || env.isWebWorker) {
     if (!globalthis.indexedDB) {
@@ -24,6 +55,6 @@ const needShim = async (): Promise<boolean> => {
 
 export const indexeddbShim = async (): Promise<void> => {
   if (await needShim()) {
-    require('fake-indexeddb/auto.js')
+    loadShim()
   }
 }
